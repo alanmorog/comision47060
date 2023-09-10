7d10a4894a2797432166a071bb5d4d54ec91d211
Alanmoro <alan.gabriel.moro@gmail.com>
2023-09-10 19:08:41 -0300

    add update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,3 @@
-/* console.log("Hola Mundo!!!")
-
-alert("Hola Mundo!!!!")
-
-documento  = prompt("Ingrese su numero de dni")
-
-alert(documento)
-console.log("Su numero de documento es: ", documento) */
-
 //definir las variables a usar
 let alumnosMenores = 0;
 let alumnosMayores = 0;
@@ -16,19 +7,15 @@ let alumnosMayores = 0;
 cantidadAlumnos = prompt("cuantos alumnos tiene el aula?: ");
 for (let i = 1; i <= cantidadAlumnos; i++) {
     let edadAlumnos = prompt("que edad tiene?: ")
-    if (edadAlumnos >= 18){
+    if (edadAlumnos >= 18) {
         alumnosMayores++;
-    }else
-    if (edadAlumnos < 18 && edadAlumnos > 0){
-        alumnosMenores++;
-    }else{
-        alert("el valor no existe, intente de nuevo...");
-        i--;
-    }
-
-
-/*   console.log(alumnosMayores)
-    console.log(alumnosMenores) */
+    } else
+        if (edadAlumnos < 18 && edadAlumnos > 0) {
+            alumnosMenores++;
+        } else {
+            alert("el valor no existe, intente de nuevo...");
+            i--;
+        }
 }
 
 //mostrar en pantalla la cantidad de alumnos
